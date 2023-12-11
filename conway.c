8f4f48788e03b761cfe4813e658dbc22218414e0
Brendon Do <brendondo1637@gmail.com>
2023-12-11 10:05:27 -0500

    fixed char to char pointer to allow calling malloc correctly

--- conway.c
+++ conway.c
@@ -5,9 +5,13 @@
 int main(void){
     Board *conway;
     conway = (Board *) malloc(sizeof(Board));
-    scanf("%d %d", &(conway->nrows), &(conway->ncols));
+    int row = 0;
+    int col = 0;
+    scanf("%d %d", &row, &col);
+    conway->nrows = row;
+    conway->ncols = col;
     conway->grid = (char *) malloc(sizeof(char)*conway->nrows*conway->ncols);
-    conway->gridrows = (char**) malloc(sizeof(char)*conway->nrows);
+    conway->gridrows = (char**) malloc(sizeof(char *)*conway->nrows);
     int numiterations = 0;
 
     for (int i = 0; i < conway->nrows; i++)
@@ -15,9 +19,11 @@ int main(void){
 	conway->gridrows[i] = conway->grid + i * conway->ncols;
     }
 
+    scanf("%d", &numiterations);
+
     for (int i = 0; i < conway->nrows; i++)
     {
-	char *temp = (char *) malloc(sizeof(char)*conway->ncols*2);
+	char *temp = (char *) malloc(sizeof(char)*conway->ncols*2+1);
 	temp = fgets(temp, conway->ncols * 2, stdin);
 	for (int j = 0; j < conway->ncols * 2; j += 2)
 	{
@@ -25,8 +31,6 @@ int main(void){
 	}
 	free(temp);
     }
-
-    scanf("%d", &numiterations);
 
     for (int i = 0; i < numiterations; i++)
     {
